107ee2b913e28e5b92e9b5a170f727603533f0f0
github-ci <you@example.com>
2025-04-28 09:47:38 +0000

    fix: 修复字段错误

--- src/Entity/MassTask.php
+++ src/Entity/MassTask.php
@@ -42,7 +42,7 @@ class MassTask
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[BoolColumn]
     #[IndexColumn]
