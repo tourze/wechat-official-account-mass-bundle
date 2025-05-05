bedc8d22ac01b0f460967b1547cd4fdf3bc60360
github-ci <you@example.com>
2025-05-05 04:05:20 +0000

    refactor(packages/wechat-official-account-mass-bundle): 移除 AdminMenu 类并更新依赖

--- src/WechatOfficialAccountMassBundle.php
+++ src/WechatOfficialAccountMassBundle.php
@@ -3,18 +3,7 @@
 namespace WechatOfficialAccountMassBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '公众号群发')]
-class WechatOfficialAccountMassBundle extends Bundle implements BundleDependencyInterface
+class WechatOfficialAccountMassBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle::class => ['all' => true],
-            \Tourze\Symfony\CronJob\CronJobBundle::class => ['all' => true],
-            \WechatOfficialAccountBundle\WechatOfficialAccountBundle::class => ['all' => true],
-        ];
-    }
 }
